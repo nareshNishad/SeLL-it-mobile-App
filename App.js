5140abc3f396a67e2e12f2ee2b1a253c13517f13
naresh-zota <naresh@zotalabs.com>
2020-12-30 11:07:24 +0530

    navigation

--- App.js
+++ App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
 
-import Login from "./screen/ListingEditScreen";
+import navigationTheme from "./app/navigation/navigationTheme";
+import AppNavigator from "./app/navigation/AppNavigator";
+import AuthNavigator from "./app/navigation/AuthNavigator";
 
 export default function App() {
-  return <Login />;
+  return (
+    <NavigationContainer theme={navigationTheme}>
+      <AppNavigator />
+    </NavigationContainer>
+  );
 }
